e55b401d8e4b334ca886ccb03bc6a08ed560e957
Scott <scmatson23@gmail.com>
2021-06-06 10:55:33 -0400

    Exercise 4 Solution

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,13 +1,14 @@
 package base;
+import java.util.Scanner;
 /*
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
  *  Copyright 2021 Scott Matson
  */
-
 /*
 Exercise 4 - Mad Lib
 
-Mad libs are a simple game where you create a story template with blanks for words. You, or another player, then construct a list of words and place them into the story, creating an often silly or funny story as a result.
+Mad libs are a simple game where you create a story template with blanks for words. You, or another player, then
+construct a list of words and place them into the story, creating an often silly or funny story as a result.
 
 Create a simple mad-lib program that prompts for a noun, a verb, an adverb, and an adjective and injects those into a story that you create.
 
@@ -29,6 +30,32 @@ Challenges
 -Add more inputs to the program to expand the story.
 -Implement a branching story, where the answers to questions determine how the story is constructed.
 */
-
 public class App {
+    public static void main(String[] args) {
+        Scanner ui = new Scanner(System.in);
+        String quote = ("Do you verb your adjective noun adverb? That's hilarious!");
+
+        System.out.print("Enter a noun: ");
+        String noun = ui.nextLine();
+
+        System.out.print("Enter a verb: ");
+        String verb = ui.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        String adjective = ui.nextLine();
+
+        System.out.print("Enter an adverb: ");
+        String adv = ui.nextLine();
+
+        quote = quote.replace("adjective", adjective);
+        quote = quote.replace("noun", noun);
+        quote = quote.replace("adverb", adv);
+        quote = quote.replace("verb", verb);
+
+        System.out.println(quote);
+
+        //Concatenation
+        //System.out.println("Do you " + verb + " your " + adjective + " " + noun + " " + adverb + "? That's hilarious!");
+
+    }
 }
